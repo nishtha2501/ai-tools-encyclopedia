9ca5f20a39c0878b64a40fcaa736b7bfb073b42b
nishtha2501 <nishthasharma.2501@gmail.com>
2026-07-07 01:02:07 +0530

    initial version of AI Tools Encyclopedia

---
+++ data/tools.js
@@ -0,0 +1,52 @@
+const tools = [
+    {
+        id: 1,
+        name: "ChatGPT",
+        category: "Writing",
+        pricing: "Freemium",
+        rating: 4.9,
+        description: "AI assistant for writing, coding and learning.",
+        website: "https://chatgpt.com",
+        logo: "images/chatgpt.png"
+    },
+    {
+        id: 2,
+        name: "Claude",
+        category: "Writing",
+        pricing: "Freemium",
+        rating: 4.9,
+        description: "Helpful AI assistant by anthropic.",
+        website: "https://claude.ai",
+        logo: "images/Claude_AI.png"
+    },
+    {
+        id: 3,
+        name: "GitHub Copilot",
+        category: "Coding",
+        pricing: "Paid",
+        rating: 4.7,
+        description: "AI pair programmer for developers.",
+        website: "https://github.com/features/copilot",
+        logo: "images/github-copilot.png"
+    },
+    {
+        id: 4,
+        name: "Gemini",
+        category: "Productivity",
+        pricing: "Freemium",
+        rating: 4.7,
+        description: "Google's AI assistant.",
+        website: "https://gemini.google.com",
+        logo: "images/Gemini_logo.png"
+    },
+    {
+        id: 5,
+        name: "Midjourney",
+        category: "Image",
+        pricing: "Paid",
+        rating: 4.8,
+        description: "Generate stunning AI artwork.",
+        website: "https://www.midjourney.com",
+        logo: "images/Midjourney.png"
+    }
+];
---
+++ js/app.js
@@ -0,0 +1,162 @@
+console.log("App.js loaded");
+console.log("App Loaded");
+console.log(tools);
+const compareBtn = document.getElementById("compare-btn");
+const compareModal = document.getElementById("compare-modal");
+const closeModal = document.getElementById("close-modal");
+const comparisonContent = document.getElementById("comparison-content");
+const toolsContainer = document.getElementById("tools-container");
+const searchInput = document.getElementById("search-input");
+const categoryCards = document.querySelectorAll(".category-card");
+const sortSelect = document.getElementById("sort-select");
+const themeBtn = document.getElementById("theme-btn");
+
+let currentSearch = "";
+let currentCategory = "All";
+let currentSort = "default";
+let favourites = JSON.parse(localStorage.getItem("favourites")) || [];
+let compareList = [];
+
+themeBtn.addEventListener("click", function(){
+    document.body.classList.toggle("dark");
+});
+
+function displayTools(toolArray){
+    toolsContainer.innerHTML = "";
+    toolArray.forEach(function(tool){
+        const card = `
+        <div class="tool-card">
+        <div class="card-top">
+            
+        <button class="favourite-btn" data-id="${tool.id}"> ${favourites.includes(tool.id) ? "❤️" :"🤍"}</button>
+        </div>
+        <label class="compare-label">
+        <input
+        type="checkbox"
+        class="compare-checkbox"
+        data-id="${tool.id}">Compare </label>
+        <img src="${tool.logo}" alt="${tool.name}" class="tool-logo">
+        <h3>${tool.name}</h3>
+        <p class="rating">⭐${tool.rating}</p>
+        <p>${tool.description}</p>
+        <span class="category-tag">${tool.category}</span>
+        <br><br>
+        <a href="${tool.website}" target= "_blank" class="visit-btn">Visit Website</a>
+        </div>
+        `;
+        toolsContainer.innerHTML += card;
+    });
+}
+displayTools(tools);
+
+function updateDisplay(){
+    let filteredTools = [...tools];
+    if(currentSearch !== ""){
+        filteredTools = filteredTools.filter(function(tool){
+            return tool.name.toLowerCase().includes(currentSearch);
+        });
+    }
+}
+
+
+searchInput.addEventListener("input", function(){
+   currentSearch = searchInput.value.toLowerCase();
+   updateDisplay();
+});
+if(currentCategory !== "All"){
+    filteredTools = filteredTools.filter(function(tool){
+        return tool.category ===currentCategory;
+    });
+}
+if(currentSort === "rating"){
+    filteredTools.sort(function(a, b){
+        return b.rating - a.rating;
+    });
+}
+if(currentSort==="name"){
+    filteredTools.sort(function(a,b){
+        return a.name.localeCompare(b.name);
+    });
+}
+categoryCards.forEach(function(card){
+    card.addEventListener("click", function(){
+        categoryCards.forEach(function(category){
+            category.classList.remove("active");
+        });
+
+       
+        card.classList.add("active");
+       currentCategory = card.dataset.category;
+        updateDisplay();
+    
+    });
+});
+
+toolsContainer.addEventListener("click", function(event){
+    if (event.target.classList.contains("favourite-btn")){
+        const toolId = Number(event.target.dataset.id);
+        if(favourites.includes(toolId)){
+            favourites = favourites.filter(function(id){
+                return id !== toolId;
+            });
+        event.target.textContent = "🤍";
+        }
+        else {
+            favourites.push(toolId);
+            event.target.textContent = "❤️";
+        }
+        console.log(favourites);
+        localStorage.setItem("favourites", JSON.stringify(favourites));
+    }
+
+    if(event.target.classList.contains("compare-checkbox")){
+        const toolId = Number(event.target.dataset.id);
+        if(event.target.checked){
+            compareList.push(toolId);
+        }
+        else{
+            compareList = compareList.filter(function(id){
+                return id !== toolId;
+            });
+        }
+        console.log(compareList);
+    }
+});
+
+sortSelect.addEventListener("change", function(){
+            let sortedTools = [...tools];
+
+           currentSort=sortSelect.value;
+            updateDisplay();
+});
+
+compareBtn.addEventListener("click", function(){
+    if(compareList.length ===0){
+        comparisonContent.innerHTML = "<p>Please select at leasr one tool.</p>";
+    }
+    else{
+        let html = "";
+
+        compareList.forEach(function(id){
+            const tool = tools.find(function(item){
+                return item.id === id;
+            });
+            html += `
+            <div class="compare-card">
+            <img src="${tool.logo}" class="yool-logo">
+            <h3>${tool.name}</h3>
+            <p>Rating: ${tool.rating}</p>
+            <p>Category: ${tool.category}</p>
+            <p>${tool.description}</p>
+            <a href="${tool.website}" target="_blank">Visit Website</a>
+            </div>
+            `;
+        });
+        comparisonContent.innerHTML = html;
+    }
+    compareModal.style.display = "flex";
+});
+
+closeModal.addEventListener("click", function(){
+    compareModal.style.display = "none";
+});
